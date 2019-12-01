91a91a94534b9a8f4ede8bef7208ea7301f59277
Kim Sangyeon <dho113@naver.com>
2019-12-01 16:03:42 +0900

    modify config

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+const external = id => !id.startsWith('/') && !id.startsWith('.');
+
+export default {
+  input: './src/useLoadTime.js',
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+  },
+  plugins: [babel({ runtimeHelpers: true })],
+  external,
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: ['@babel/polyfill', './example/App.jsx'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'example'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  mode: 'production',
+  plugins: [],
+  devServer: {
+    contentBase: './example',
+    host: '0.0.0.0',
+  },
+};
